f70a8a4007a0a65d3fb80d9d7d5ad2151c96ad51
Sulhan Jauhary <ms.jauhary@gmail.com>
2020-04-12 14:33:57 +0700

    added style for default page

--- src/page/app.js
+++ src/page/app.js
@@ -1,17 +1,20 @@
-import Vue from "vue";
-import App from "./App.vue";
-import axios from "axios";
+import Vue from 'vue';
+import App from './App.vue';
+import axios from 'axios';
+import './app.scss';
 
 Vue.config.productionTip = false;
 
-global.browser = require("webextension-polyfill");
+global.browser = require('webextension-polyfill');
 Vue.prototype.$browser = global.browser;
+Vue.prototype.$isEmpty = data =>
+	Boolean(data && data.constructor == Object && Object.keys(data) <= 0);
 Vue.prototype.$axios = axios.create({
-  headers: {
-    Authorization: "Client-ID " + process.env.UNSPASH_ACCESS_KEY
-  }
+	headers: {
+		Authorization: 'Client-ID ' + process.env.UNSPASH_ACCESS_KEY
+	}
 });
 
 new Vue({
-  render: h => h(App)
-}).$mount("#app");
+	render: h => h(App)
+}).$mount('#app');
